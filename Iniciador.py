39e89935041cdc940b5051e7eb4d653d0eab5364
victor071298 <87611661+victor071298@users.noreply.github.com>
2022-06-28 00:45:38 -0300

    Update Iniciador.py

--- Iniciador.py
+++ Iniciador.py
@@ -1,16 +1,14 @@
 from sys import exit
 import rpyc
 
-while True:
-    eleicao = input("Pronto para iniciar a votação, indique o id do processo desejado e o tipo de eleicao que deseja executar,respectivamente(M para maior id e m para menor id separados por espaço)\nCaso deseje encerrar a eleicao, digite N\n")
-    if eleicao[0] == 'N':
-        exit()
-    eleicao = eleicao.split(" ")
-    conn = rpyc.connect('localhost', 5000+int(eleicao[0]))
-    resultado = conn.root.exposed_election(eleicao[1])
-    print("eleicao concluida, com lider eleito sendo:{}".format(resultado))
-    conn.close()
-    #Resetando os valores
-    #conn = rpyc.connect('localhost', 5000+int(eleicao[0]))
-    #resultado = conn.root.exposed_reset(eleicao[1])
-    #conn.close()
+#iniciando eleição 
+eleicao = input("Pronto para iniciar a votação, indique o id do processo desejado e o tipo de eleicao que deseja executar,respectivamente(M para maior id e m para menor id separados por espaço)\nCaso deseje encerrar a eleicao, digite N\n")
+#encerra caso o input seja 'N'
+if eleicao[0] == 'N':
+    exit()
+#dividindo os termos da eleicao passados como input    
+eleicao = eleicao.split(" ")
+conn = rpyc.connect('localhost', 5000+int(eleicao[0]))
+resultado = conn.root.exposed_election(eleicao[1])
+print("eleicao concluida, com lider eleito sendo:{}".format(resultado))
+conn.close()
